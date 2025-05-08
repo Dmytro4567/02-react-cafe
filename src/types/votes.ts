5e701e8dbcbce5f0393e1b99124ace79765117fa
Dmytro <dmitry.dvernikov@gmail.com>
2025-05-08 23:45:37 +0300

    add normalize

--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,7 +1,5 @@
-// допустимі значення для голосування
 export type VoteType = 'good' | 'neutral' | 'bad';
 
-// інтерфейс для стану голосів
 export interface Votes {
     good: number;
     neutral: number;
